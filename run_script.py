907de196dd618727c10d354d036b90a63349bd2f
Kendrick Bong <kendrick.bong@educationhorizons.com>
2022-04-05 12:25:13 +0800

    Yeet

--- run_script.py
+++ run_script.py
@@ -1,7 +1,12 @@
 import git
 from git_contributions_importer import *
 
-repos_path = ["../MyPassGlobal/mypass-global-suite/","../mypass-domains"]
+repos_path = ["../local-development-docker/ss-identity-service/",
+            "../local-development-docker/ss-notification-connection-lambda/", 
+            "../local-development-docker/staff-app/",
+            "../local-development-docker/ss-notification-service/",
+            "../local-development-docker/ss-sts-access-lib/",
+            "../local-development-docker/sts-token-lib/"]
 repos=[]
 
 for repo_path in repos_path:
@@ -10,5 +15,5 @@ for repo_path in repos_path:
 mock_repo_path = "./MockRepo"
 mock_repo = git.Repo.init(mock_repo_path)
 importer = Importer(repos, mock_repo)
-importer.set_author(['Kendrickbong1996@gmail.com', 'kbong@mypassglobal.com'])
+importer.set_author(['Kendrickbong1996@gmail.com', 'kendrick.bong@educationhorizons.com'])
 importer.import_repository()
